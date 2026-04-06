59809468ab0bc6765cc66ccea3cfc858c7c6e271
Pratik-singh <pratiksingh8591@gmail.com>
2026-04-07 01:42:51 +0530

    added features

--- routes/host.js
+++ routes/host.js
@@ -4,15 +4,13 @@ const path=require('path')
 const rootdir=require('../utils/pathutil')
 
   hostRouter.get("/add-home",(req,res,next)=>{
-  res.sendFile(path.join(__dirname,'../','views','book.html'))
+  res.sendFile(path.join(rootdir,'views','book.html'))
  })
 hostRouter.post("/add-home",(req,res,next)=>{
     console.log("deatils",req.body);
     const name=req.body.name;
     const housename=req.body['house-name'];
-    res.send(`<h1>thank you ${name} for chosing Air-bnb</h1>
-        <p>your house ${housename} is our house from now </p>
-      <p>WE ARE A FAMILY NOW</p>  `)
+    res.sendFile(path.join(rootdir,'views','addedhome.html'))
 
  })
  module.exports=hostRouter;
